8b7ddcd6f9df7760b3b2ed1e95437293dbedb2db
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-08-05 19:55:55 +0900

    Update page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'ITS健保（関東ITソフトウェア健康保険組合）の施設空き状況カレンダー'
+    title: '空き状況カレンダー | ITS健保（関東ITソフトウェア健康保険組合）施設検索'
   });
 });
 
