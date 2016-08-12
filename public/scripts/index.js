447516cca9231aadc3fb1cab844d6a46d8dc8a28
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-08-12 09:58:43 +0900

    Allow to filter hotels via checkboxes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,11 +9,16 @@ $(document).ready(function() {
     dataType: 'json',
     cache: false,
     success: function(data) {
-      var hotels = [];
+      var hotels = {};
       data.forEach(function(hotel) {
-        hotels.push(hotel.name);
+        hotels[hotel.name] = {
+          name: hotel.name,
+          eventCount: 0
+        };
         hotel.monthlyAvailabilities.forEach(function(monthlyAvailability) {
+          console.log(monthlyAvailability);
           monthlyAvailability.dates.forEach(function(date) {
+            hotels[hotel.name].eventCount++;
             events.push({
               title: hotel.name,
               start: date,
@@ -23,7 +28,7 @@ $(document).ready(function() {
         });
       });
 
-      createSelectOptions(hotels);
+      createCheckboxes(hotels);
       createCalendar();
     },
     error: function(xhr, status, err) {
@@ -31,9 +36,34 @@ $(document).ready(function() {
     }
   });
 
-  function createSelectOptions(hotels) {
-    hotels.forEach(function(hotel) {
-      $('#hotels').append('<option value="' + hotel + '">' + hotel + '</option>');
+  function createCheckboxes(hotels) {
+    Object.keys(hotels).forEach(function(key) {
+      var hotel = hotels[key];
+      var html;
+      if (hotel.eventCount === 0) {
+        html = [
+          '<label style="padding-right: 15px; color: #ccc;">',
+          '<input type="checkbox" disabled="disabled" value="' + key + '">' + hotel.name + ' (' + hotel.eventCount + ')',
+          '</label>'
+        ].join('');
+      } else {
+        html = [
+          '<label style="padding-right: 15px;">',
+          '<input type="checkbox" value="' + key + '" checked="checked">' + hotel.name + ' (' + hotel.eventCount + ')',
+          '</label>'
+        ].join('');
+      }
+      $('#filter-hotel-checkboxes').append(html);
+    });
+
+    $('#filter-hotel-checkboxes').on('change', 'input[type="checkbox"]', function() {
+      var checkedHotelNames = $("#filter-hotel-checkboxes input[type='checkbox']")
+        .filter(function() {
+          return this.checked;
+        }).map(function() {
+          return this.value;
+        }).get();
+      filterEvents(checkedHotelNames);
     });
   }
 
@@ -52,19 +82,11 @@ $(document).ready(function() {
     });
   }
 
-  function filterEvents(hotelName) {
+  function filterEvents(hotelNames) {
     var filteredEvents = events.filter(function(event) {
-      if (hotelName === 'None') {
-        return true;
-      }
-      return event.title === hotelName;
+      return hotelNames.indexOf(event.title) > -1;
     })
     $('#calendar').fullCalendar('removeEvents');
     $('#calendar').fullCalendar('addEventSource', filteredEvents);
   }
-
-  $('#hotels').change(function() {
-    filterEvents(this.value);
-    // console.log(this.value);
-  });
 });
