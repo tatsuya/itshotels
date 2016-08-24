f19703253f1e70e44dc2afff384a402598a09760
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-08-24 21:48:06 +0900

    Simplify prefecture name mapping

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,110 +1,4 @@
 $(document).ready(function() {
-
-  var areas = {
-    'トスラブ湯沢': {
-      prefecture: {
-        code: 'JP-15',
-        name_ja: '新潟県'
-      }
-    },
-    'トスラブ箱根ビオーレ': {
-      prefecture: {
-        code: 'JP-14',
-        name_ja: '神奈川県'
-      }
-    },
-    'トスラブ箱根和奏林': {
-      prefecture: {
-        code: 'JP-14',
-        name_ja: '神奈川県'
-      }
-    },
-    'トスラブ館山ルアーナ': {
-      prefecture: {
-        code: 'JP-12',
-        name_ja: '千葉県'
-      }
-    },
-    'ブルーベリーヒル勝浦': {
-      prefecture: {
-        code: 'JP-12',
-        name_ja: '千葉県'
-      }
-    },
-    'ホテルハーヴェスト　スキージャム勝山': {
-      prefecture: {
-        code: 'JP-18',
-        name_ja: '福井県'
-      }
-    },
-    'ホテルハーヴェスト伊東': {
-      prefecture: {
-        code: 'JP-22',
-        name_ja: '静岡県'
-      }
-    },
-    'ホテルハーヴェスト南紀田辺': {
-      prefecture: {
-        code: 'JP-30',
-        name_ja: '和歌山県'
-      }
-    },
-    'ホテルハーヴェスト斑尾': {
-      prefecture: {
-        code: 'JP-20',
-        name_ja: '長野県'
-      }
-    },
-    'ホテルハーヴェスト旧軽井沢':{
-      prefecture: {
-        code: 'JP-20',
-        name_ja: '長野県'
-      }
-    },
-    'ホテルハーヴェスト那須': {
-      prefecture: {
-        code: 'JP-09',
-        name_ja: '栃木県'
-      }
-    },
-    'ホテル日航プリンセス京都': {
-      prefecture: {
-        code: 'JP-26',
-        name_ja: '京都府'
-      }
-    },
-    'リゾートホテル蓼科': {
-      prefecture: {
-        code: 'JP-20',
-        name_ja: '長野県'
-      }
-    },
-    '中沢ヴィレッジ': {
-      prefecture: {
-        code: 'JP-10',
-        name_ja: '群馬県'
-      }
-    },
-    '琵琶レイクオーツカ': {
-      prefecture: {
-        code: 'JP-25',
-        name_ja: '滋賀県'
-      }
-    },
-    '角間温泉　岩屋館': {
-      prefecture: {
-        code: 'JP-20',
-        name_ja: '長野県'
-      }
-    },
-    '鎌倉パークホテル': {
-      prefecture: {
-        code: 'JP-14',
-        name_ja: '神奈川県'
-      }
-    }
-  };
-
   var events = [];
 
   $.when($.ajax({
@@ -116,10 +10,12 @@ $(document).ready(function() {
     dataType: 'json',
     cache: false
   })).then(function success(hotelsData, prefecturesData) {
+    // TODO: Remove prefecturesData as it's not used
     var hotels = {};
     hotelsData[0].forEach(function(hotel) {
       hotels[hotel.name] = {
         name: hotel.name,
+        prefecture: hotel.prefecture,
         eventCount: 0
       };
       hotel.monthlyAvailabilities.forEach(function(monthlyAvailability) {
@@ -134,44 +30,36 @@ $(document).ready(function() {
       });
     });
 
-    createCheckboxes(hotels, prefecturesData[0]);
+    createCheckboxes(hotels);
     createCalendar();
   }, function error(xhr, status, err) {
     console.error(url, status, err.toString());
   });
 
-  function getPrefecture(prefectures, code) {
-    for (var i = 0; i < prefectures.length; i++) {
-      var prefecture = prefectures[i];
-      if (prefecture.code === code) {
-        return prefecture;
-      }
-    }
-    return null;
-  }
-
-  function createCheckboxes(hotels, prefectures) {
+  function createCheckboxes(hotels) {
 
     var hotelPrefectures = {};
 
     Object.keys(hotels).forEach(function(key) {
       var hotel = hotels[key];
-      var area = areas[hotel.name];
-      var prefectureCode = area.prefecture.code;
+      var prefectureCode = hotel.prefecture.code;
       if (!hotelPrefectures[prefectureCode]) {
-        hotelPrefectures[prefectureCode] = [];
+        hotelPrefectures[prefectureCode] = {
+          name_ja: hotel.prefecture.name_ja,
+          hotels: []
+        };
       }
-      hotelPrefectures[prefectureCode].push(hotel);
+      hotelPrefectures[prefectureCode].hotels.push(hotel);
     });
 
     var html = [];
     html.push('<ul class="list-reset">');
     Object.keys(hotelPrefectures).sort().forEach(function(prefectureCode) {
-      var prefecture = getPrefecture(prefectures, prefectureCode);
-      var hotels = hotelPrefectures[prefectureCode];
+      var hotels = hotelPrefectures[prefectureCode].hotels;
+      var prefectureName = hotelPrefectures[prefectureCode].name_ja;
       html.push('<li>');
       html.push('<ul class="list-reset">');
-      html.push('<li class="inline-block mr2"><span class="bold">' + prefecture.name_ja + '</span></li>');
+      html.push('<li class="inline-block mr2"><span class="bold">' + prefectureName + '</span></li>');
       hotels.forEach(function(hotel) {
         if (hotel.eventCount === 0) {
           html.push([
--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -3,12 +3,120 @@ var router = express.Router();
 
 var Hotels = require('../../lib/model/hotels');
 
+var areas = {
+  'トスラブ湯沢': {
+    prefecture: {
+      code: 'JP-15',
+      name_ja: '新潟県'
+    }
+  },
+  'トスラブ箱根ビオーレ': {
+    prefecture: {
+      code: 'JP-14',
+      name_ja: '神奈川県'
+    }
+  },
+  'トスラブ箱根和奏林': {
+    prefecture: {
+      code: 'JP-14',
+      name_ja: '神奈川県'
+    }
+  },
+  'トスラブ館山ルアーナ': {
+    prefecture: {
+      code: 'JP-12',
+      name_ja: '千葉県'
+    }
+  },
+  'ブルーベリーヒル勝浦': {
+    prefecture: {
+      code: 'JP-12',
+      name_ja: '千葉県'
+    }
+  },
+  'ホテルハーヴェスト　スキージャム勝山': {
+    prefecture: {
+      code: 'JP-18',
+      name_ja: '福井県'
+    }
+  },
+  'ホテルハーヴェスト伊東': {
+    prefecture: {
+      code: 'JP-22',
+      name_ja: '静岡県'
+    }
+  },
+  'ホテルハーヴェスト南紀田辺': {
+    prefecture: {
+      code: 'JP-30',
+      name_ja: '和歌山県'
+    }
+  },
+  'ホテルハーヴェスト斑尾': {
+    prefecture: {
+      code: 'JP-20',
+      name_ja: '長野県'
+    }
+  },
+  'ホテルハーヴェスト旧軽井沢':{
+    prefecture: {
+      code: 'JP-20',
+      name_ja: '長野県'
+    }
+  },
+  'ホテルハーヴェスト那須': {
+    prefecture: {
+      code: 'JP-09',
+      name_ja: '栃木県'
+    }
+  },
+  'ホテル日航プリンセス京都': {
+    prefecture: {
+      code: 'JP-26',
+      name_ja: '京都府'
+    }
+  },
+  'リゾートホテル蓼科': {
+    prefecture: {
+      code: 'JP-20',
+      name_ja: '長野県'
+    }
+  },
+  '中沢ヴィレッジ': {
+    prefecture: {
+      code: 'JP-10',
+      name_ja: '群馬県'
+    }
+  },
+  '琵琶レイクオーツカ': {
+    prefecture: {
+      code: 'JP-25',
+      name_ja: '滋賀県'
+    }
+  },
+  '角間温泉　岩屋館': {
+    prefecture: {
+      code: 'JP-20',
+      name_ja: '長野県'
+    }
+  },
+  '鎌倉パークホテル': {
+    prefecture: {
+      code: 'JP-14',
+      name_ja: '神奈川県'
+    }
+  }
+};
+
 router.get('/', function(req, res, next) {
     Hotels.get(function(err, hotels) {
     if (err) {
       return next(err);
     }
-    res.json(hotels.data);
+    res.json(hotels.data.map(function(hotel) {
+      hotel.prefecture = areas[hotel.name].prefecture;
+      return hotel;
+    }));
   });
 });
 
