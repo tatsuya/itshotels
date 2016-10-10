8f8fe23b6a2ddd2eb973a71192195b9cf446a7e9
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-10-11 08:24:58 +0900

    Hide links to each hotel from header

--- routes/hotels.js
+++ routes/hotels.js
@@ -4,25 +4,35 @@ var router = express.Router();
 var Hotels = require('../lib/model/hotels');
 
 router.get('/', function(req, res, next) {
-  res.render('hotels', {
-    path: '/hotels',
-    title: '施設一覧 | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
-    images: Hotels.listImages(),
-    hotels: Hotels.listNamesAndPaths()
+  Hotels.listNamesAndPaths((err, hotels) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('hotels', {
+      path: '/hotels',
+      title: '施設一覧 | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
+      images: Hotels.listImages(),
+      hotels: hotels
+    });
   });
 });
 
 router.get('/:hotelKey', function(req, res, next) {
   let hotelKey = req.params.hotelKey;
-  Hotels.getOne(hotelKey, function(err, hotel) {
+  Hotels.getOne(hotelKey, (err, hotel) => {
     if (err) {
       return next(err);
     }
-    res.render('hotels/' + hotelKey, {
-      path: '/hotels/' + hotelKey,
-      title: hotel.name + ' | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
-      hotel: hotel,
-      hotels: Hotels.listNamesAndPaths()
+    Hotels.listNamesAndPaths((err2, hotels) => {
+      if (err2) {
+        return next(err2);
+      }
+      res.render('hotels/' + hotelKey, {
+        path: '/hotels/' + hotelKey,
+        title: hotel.name + ' | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
+        hotel: hotel,
+        hotels: hotels
+      });
     });
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@ var router = express.Router();
 var Hotels = require('../lib/model/hotels');
 
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    path: '/',
-    title: '空き状況カレンダー | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
-    hotels: Hotels.listNamesAndPaths()
+  Hotels.listNamesAndPaths((err, hotels) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('index', {
+      path: '/',
+      title: '空き状況カレンダー | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
+      hotels: hotels
+    });
   });
 });
 
