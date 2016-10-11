3a72df59e2b87e285945b6d1b279550ab6a7a0e1
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-10-12 08:18:53 +0900

    Render partials at once

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,7 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var pug = require('pug');
+var path = require('path');
+
 var Hotels = require('../lib/model/hotels');
+
+const hotelKeys = [
+  'biwako',
+  'ito',
+  'iwaya',
+  'kamakura',
+  'katsuura',
+  'katsuyama',
+  'kyoto',
+  'kyukaruizawa',
+  'madarao',
+  'nakazawa',
+  'nanki',
+  'nasu',
+  'tateshina',
+  'toslove_luana',
+  'toslove_viole',
+  'toslove_wasorin',
+  'toslove_yuzawa'
+];
+
+const partials = hotelKeys.reduce((map, key) => {
+  map[key] = renderPartial(key);
+  return map;
+}, {});
+
+function renderPartial(key) {
+  return pug.renderFile(path.join(__dirname, '../views/partials/', key + '.pug'));
+}
 
 router.get('/', function(req, res, next) {
   Hotels.listNamesAndPaths((err, hotels) => {
@@ -27,11 +59,12 @@ router.get('/:hotelKey', function(req, res, next) {
       if (err2) {
         return next(err2);
       }
-      res.render('hotels/' + hotelKey, {
+      res.render('hotel', {
         path: '/hotels/' + hotelKey,
         title: hotel.name + ' | ITS健保（関東ITソフトウェア健康保険組合）施設検索',
         hotel: hotel,
-        hotels: hotels
+        hotels: hotels,
+        html: partials[hotelKey]
       });
     });
   });
